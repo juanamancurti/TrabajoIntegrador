cea3e861878d43e45725f270c9f587b0ad088450
juanagrether <jgrether@udesa.edu.ar>
2023-11-13 14:52:02 -0300

    index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 //Header y footer
+let ApiKey=  "1173214cf5e2ac8f2c0ac1c242d0ec8a";
+let TopRated=  `https://api.themoviedb.org/3/movie/top_rated?api_key=${ApiKey}`;
+let PeliculasPopulares= `https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}`
+let SereiesPopulares= `https://api.themoviedb.org/3/tv/popular?api_key=${acaVaLaAPIKey}`
+
 
 
 //Resultado de Busqueda
